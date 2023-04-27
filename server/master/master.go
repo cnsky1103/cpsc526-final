52360e6a9ff5f711e3316218917da2591e1ec53c
cnsky1103 <cnsky1103@gmail.com>
2023-04-27 16:25:31 -0700

    impl GetTableByKey

--- server/master/master.go
+++ server/master/master.go
@@ -17,7 +17,7 @@ type MasterServer struct {
 	cli     *clientv3.Client
 	servers []string // store all server ips
 
-	// tablets []string // store all tablets existing
+	tablets []string // store all tablets existing
 	// tablets_for_table map[string][]string // k: table, v: tablets for this table
 }
 
@@ -51,7 +51,17 @@ func (this *MasterServer) watchServers() {
 }
 
 func (this *MasterServer) GetTabletByKey(ctx context.Context, req *proto.GetTabletByKeyRequest) (*proto.GetTabletByKeyResponse, error) {
-	return &proto.GetTabletByKeyResponse{TabletName: "tablet1"}, nil
+	name := ""
+	for i := 0; i < len(this.tablets); i++ {
+		if i == len(this.tablets)-1 {
+			name = this.tablets[i]
+		} else {
+			if this.tablets[i] >= req.Key && this.tablets[i+1] < req.Key {
+				name = this.tablets[i]
+			}
+		}
+	}
+	return &proto.GetTabletByKeyResponse{TabletName: name}, nil
 }
 
 func main() {
