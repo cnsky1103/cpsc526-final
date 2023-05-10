de87b953a84552f4da8e5e08fa3b503d114d8a9e
Yibo Yan <totoroq@outlook.com>
2023-05-10 03:51:37 -0400

    fill out client

--- client/client.go
+++ client/client.go
@@ -7,6 +7,8 @@ import (
 	"log"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Client struct {
@@ -14,46 +16,107 @@ type Client struct {
 	master_conn proto.MasterServiceClient
 }
 
-func NewClient() *Client {
+func NewClient() (*Client, error) {
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   config.Endpoints,
 		DialTimeout: config.DialTimeout,
 	})
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return nil, err
 	}
+
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.Dial(config.MasterServerIp, opts...)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
 	return &Client{
-		etcdCli: cli,
-	}
+		etcdCli:     cli,
+		master_conn: proto.NewMasterServiceClient(conn),
+	}, nil
 }
 
-func (client *Client) GetTabletForKey(key string) string {
-	root_tablet, err := client.etcdCli.Get(context.Background(), "root_tablet")
+func (client *Client) GetTabletServerIpForKey(key string) (string, error) {
+	// root_tablet, err := client.etcdCli.Get(context.Background(), "root_tablet")
+	resp, err := client.master_conn.GetTabletByKey(
+		context.Background(),
+		&proto.GetTabletByKeyRequest{Key: key},
+	)
 
 	if err != nil {
-
+		log.Printf("Failed to get tablet name from master server: %v\n", err)
+		return "", err
 	}
 
-	/*
-		find root tablet server, get the user table for key
-		return user table address
-	*/
-
-	return ""
+	return resp.ServerIp, nil
 }
 
-func (client *Client) Get(key string) string {
-	tablet_path := client.GetTabletForKey(key)
-	/*
-		call server.Get()
-	*/
-	return ""
+func (client *Client) Get(key string) (string, error) {
+	serverIp, err := client.GetTabletServerIpForKey(key)
+
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	tabletClient, err := establishTabletClient(serverIp)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	resp, err := tabletClient.Get(context.Background(), &proto.GetRequest{Key: key})
+
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	return resp.Value, nil
 }
 
-func (client *Client) Put(key string, val string) string {
-	tablet_path := client.GetTabletForKey(key)
-	/*
-		call server.Get()
-	*/
-	return ""
+func (client *Client) Set(key string, val string) error {
+	serverIp, err := client.GetTabletServerIpForKey(key)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	tabletClient, err := establishTabletClient(serverIp)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	_, err = tabletClient.Set(context.Background(), &proto.SetRequest{
+		Key:   key,
+		Value: val,
+	})
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func establishTabletClient(ip string) (proto.TabletServiceClient, error) {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.Dial(ip, opts...)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	tabletClient := proto.NewTabletServiceClient(conn)
+	return tabletClient, nil
 }
